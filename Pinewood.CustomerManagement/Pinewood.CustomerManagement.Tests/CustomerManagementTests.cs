33d751287721a7072b55533005918fa57204b256
owais <owaisvaiyani@gmail.com>
2024-06-09 19:17:01 +0100

    added unit test

--- Pinewood.CustomerManagement/Pinewood.CustomerManagement.Tests/CustomerManagementTests.cs
+++ Pinewood.CustomerManagement/Pinewood.CustomerManagement.Tests/CustomerManagementTests.cs
@@ -4,17 +4,55 @@ using Pinewood.CustomerManagement.Core.Context;
 using Microsoft.EntityFrameworkCore;
 using Pinewood.CustomerManagement.Core.Entities;
 using Pinewood.CustomerManagement.Core.Repository;
-using System.Threading;
 
 namespace Pinewood.CustomerManagement.Tests
 {
     [TestFixture]
     public class CustomerControllerTests
     {
+        private CustomerManagementRepository _repository;       
+
         [Test]
-        public void Some_Test_Should_Do_Something()
+        public void AddCustomer_Should_Add_Customer_To_Context()
         {
-            
-        }       
+            var newCustomer = new Customer { Id = 1, Name = "John Doe", Email = "john@example.com" };            
+            _repository = new CustomerManagementRepository(new TestDbContextFactory());
+
+            var result = _repository.AddCustomer(newCustomer);
+
+            Assert.IsTrue(result);           
+        }
+
+        [Test]
+        public void AddCustomer_Should_Add_Customer_And_Save()
+        {  
+            var newCustomer = new Customer { Id = 1, Name = "John Doe", Email = "john@example.com" };
+            _repository = new CustomerManagementRepository(new TestDbContextFactory());
+
+            _repository.AddCustomer(newCustomer);
+
+            var result = _repository.GetCustomer(newCustomer.Id);
+
+            Assert.AreEqual(result.Id, newCustomer.Id);
+            Assert.AreEqual(result.Name, newCustomer.Name);
+            Assert.AreEqual(result.Email, newCustomer.Email);
+        }
+    }
+
+    public class TestDbContextFactory : IDbContextFactory<CustomerContext>
+    {
+        private DbContextOptions<CustomerContext> _options;
+
+        public TestDbContextFactory(string databaseName = "InMemoryTest")
+        {
+            _options = new DbContextOptionsBuilder<CustomerContext>()
+                .UseInMemoryDatabase(databaseName)
+                .Options;
+        }
+
+        public CustomerContext CreateDbContext()
+        {
+            return new CustomerContext(_options);
+        }
     }
 }
